5a56284a64f7508e87f23a806db03f50c88c3fec
Elijah Kimani <elijahkimani1293@gmail.com>
2025-11-22 13:47:16 +0300

    Add ustwo.png logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+//   base: "./",
+//   build:{
+//     outDir:"dist",
+//   },
+// });
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "./",
-  build:{
-    outDir:"dist",
+  base: "/", // Important! Not "./"
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Ensure assets go to /assets
   },
 });
