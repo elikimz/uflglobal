940792d335b57b2e4767174bc41df85959ed928c
elikimz <elijahkimani1293@gmail.com>
2026-01-19 23:51:28 +0300

    commit

--- src/features/withdrawal/adminupdatewallet.tsx
+++ src/features/withdrawal/adminupdatewallet.tsx
@@ -519,9 +519,10 @@ const AdminWalletManagement: React.FC = () => {
                 </button>
                 <button
                   onClick={handleUpdateWallet}
-                  className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors"
+                  className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-750 transition-colors"
                 >
-                  Save Changes
+                  Save
+                   Changes
                 </button>
               </div>
             </motion.div>
